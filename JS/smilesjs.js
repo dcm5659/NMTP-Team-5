b5b9ed53d9de6afd50d4e1bbfd2355fbfadfbc62
Dan Martin <dcm5659@rit.edu>
2017-04-05 22:12:59 -0400

    resized gallery section added title  and button to gallery removed arrows from carousel added scrolling function to buttons - JS css reusability changes - button class

--- JS/smilesjs.js
+++ JS/smilesjs.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
   $('.carousel').slick({
-  	arrows:true,
+  	arrows:false,
   	dots:true
   });
+
+  $('.scroll').click(function() {
+      $('html, body').stop().animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+      }, 1000);
+    });
+
 });
 			
