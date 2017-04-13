75cce9606b83ef23972c4aa84edcba4e2372da06
Dan Martin <dcm5659@rit.edu>
2017-04-12 20:55:54 -0400

    Removed extraneous function from myjs Finished responsive design for page - updated html/css Updated reactivity for blog section

--- JS/smilesjs.js
+++ JS/smilesjs.js
@@ -9,23 +9,10 @@ $(document).ready(function(){
         scrollTop: $( $(this).attr('href') ).offset().top
       }, 1000);
     });
-
-  resizeimg();
 });
 			
 
 $( window ).resize(function() {
   console.log($(window).width());
-  resizeimg();
 });
 
-function resizeimg(){
-  if($(window).width() < 1200){
-    var image = document.getElementById('collectionimg');
-    var newheight = image.clientHeight + 470;
-    $('#datacollection').css('height', newheight);
-    $('#collectionimg').css({'position': 'absolute','top': '470px'})
-  } else {
-    $('#datacollection').removeAttr('style');
-  }
-}
